6e85ecddcb7f5e4bec536716d1d358e582cb10ac
mriwilis <mriwilis@gmail.com>
2021-07-31 12:30:21 -0300

    Scrapeo todas las paginas

--- properapi.py
+++ properapi.py
@@ -3,33 +3,63 @@ from bs4 import BeautifulSoup
 import requests
 import unicodedata
 
-url = "https://www.properati.com.ar/s/palermo/departamento/alquiler?page=1"
-response = requests.get(url)
-response.encoding = "utf-8"
-html = response.text
-dom = BeautifulSoup(html, features = "html.parser")
 
-anuncios = dom.find_all( attrs = { 'class' : 'StyledCard-n9541a-1 jWSYcc' } )
+def pagina(num):
+    url = "https://www.properati.com.ar/s/palermo/departamento/alquiler?page=" + str(num)
+    response = requests.get(url)
+    response.encoding = "utf-8"
+    html = response.text
+    dom = BeautifulSoup(html, features = "html.parser")
+    fin = dom.find( attrs = { 'class' : 'StyledPagerButton-sq1exe-0 cFkGqv pager-next' } )
+    return fin, dom
 
-print(anuncios)
+row=1
+lista_propiedades=[]
+num=1
+fin, dom = pagina(num)
 
-for anuncio in anuncios:
+
+while fin != None:
     
-    titulo = anuncio.find( attrs = { 'class' : 'StyledTitle-n9541a-4 bwJAej' } )
-    precio = anuncio.find( attrs = { 'class' : 'StyledPrice-sc-1wixp9h-0 bZCCaW' } )
-    expensas = anuncio.find( attrs = { 'class' : 'StyledMaintenanceFees-n9541a-6 cRsmn' } )
-    detalles = anuncio.find( attrs = { 'class' : 'StyledInfoIcons-n9541a-9 fgcFIO' } )
-    inmobiliaria = anuncio.find( attrs = { 'class' : 'seller-name' } )
+    anuncios = dom.find_all( attrs = { 'class' : 'StyledCard-n9541a-1 jWSYcc' } )
     
-    if titulo: titulo = titulo.get_text()
-    if precio: precio= precio.get_text()
-    if expensas: expensas = unicodedata.normalize("NFKD", expensas.get_text()) 
-    if inmobiliaria: imobiliaria = inmobiliaria.get_text()
-    if detalles:
-        spans = detalles.find_all('span')
-        for span in spans:
-            txt = span.get_text()
-            if (txt.find('m²')>=0): m2 = txt
-            if (txt.find('ambiente')>=0): ambientes = txt
-            if (txt.find('baño')>=0): banios = txt
+    for anuncio in anuncios:
+        row=row
+        titulo = anuncio.find( attrs = { 'class' : 'StyledTitle-n9541a-4 bwJAej' } )
+        precio = anuncio.find( attrs = { 'class' : 'StyledPrice-sc-1wixp9h-0 bZCCaW' } )
+        expensas = anuncio.find( attrs = { 'class' : 'StyledMaintenanceFees-n9541a-6 cRsmn' } )
+        detalles = anuncio.find( attrs = { 'class' : 'StyledInfoIcons-n9541a-9 fgcFIO' } )
+        inmobiliaria = anuncio.find( attrs = { 'class' : 'seller-name' } )
+        url = 'https://www.properati.com.ar/' + anuncio.find( attrs = { 'class' : 'StyledCardInfo-n9541a-2 ctwAhK' } ).a['href']
+        
+        if titulo: titulo = titulo.get_text()
+        if precio: precio= precio.get_text()
+        if expensas: expensas = unicodedata.normalize("NFKD", expensas.get_text()) 
+        if inmobiliaria: imobiliaria = inmobiliaria.get_text()
+        if detalles:
+            spans = detalles.find_all('span')
+            for span in spans:
+                txt = span.get_text()
+                if (txt.find('m²')>=0): m2 = txt
+                if (txt.find('ambiente')>=0): ambientes = txt
+                if (txt.find('baño')>=0): banios = txt
+    
+        dicc_propiedad = {'id':row,
+                          'titulo':titulo,
+                          'precio':precio,
+                          'expensas':expensas,
+                          'inmobiliaria':inmobiliaria,
+                          'detalles':detalles,
+                          'link':url}
+        
+        lista_propiedades.append(dicc_propiedad)
+        row = row + 1
+    
+    num = num + 1
+    
+    fin , dom = pagina(num)
+    
+    
+        
+
 
